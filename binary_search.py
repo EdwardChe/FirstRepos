2ccb5eb5efdbcf090c3e235fa56b7227861e41e9
Ed_Che <bj-spb@mail.ru>
2020-04-16 14:40:07 +0300

    add new file

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(list, item):
+  low = 0
+  high = len(list) - 1
+
+  
+  while low <= high:
+    
+    mid = (low + high) // 2
+    guess = list[mid]
+    
+    if guess == item:
+      return mid
+    
+    if guess > item:
+      high = mid - 1
+    
+    else:
+      low = mid + 1
+
+ 
+search_number = int(input('Введите чило для поиска '))
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, search_number))
+
